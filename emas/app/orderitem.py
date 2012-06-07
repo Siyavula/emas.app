fe31784d9cc3a25f0097f0962195cffcc476d963
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-06-07 10:41:24 +0200

    Removed unused imports and ancestor.

--- emas/app/orderitem.py
+++ emas/app/orderitem.py
@@ -5,23 +5,16 @@ from zope import schema
 from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
-from zope.interface import invariant, Invalid
-
 from z3c.form import group, field
-
-from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
-from plone.app.textfield import RichText
-
 from z3c.relationfield.schema import RelationList, RelationChoice
+
 from plone.formwidget.contenttree import ObjPathSourceBinder
+from plone.app.textfield import RichText
 
 from emas.app import MessageFactory as _
 
 
-class IOrderItem(form.Schema, IImageScaleTraversable):
+class IOrderItem(form.Schema):
     """
     An order line item.
     """
