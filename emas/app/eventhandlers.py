5a72fae5ad8b8c3ff1f3838c638a8acb4f24109e
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-06-21 22:04:06 +0200

    Move eventhandlers for credit management to emas.app

--- emas/app/eventhandlers.py
+++ emas/app/eventhandlers.py
@@ -11,6 +11,11 @@ from Products.ATContentTypes.permission import ModifyViewTemplate
 from Products.ATContentTypes.content.folder import ATFolder
 from Products.ATContentTypes.lib.constraintypes import ENABLED
 
+from emas.theme.browser.views import is_expert
+
+from emas.app.browser.utils import member_services
+from emas.app.browser.utils import qaservice_uuids
+
 
 def orderAdded(order, event):
     """ Set the userid
@@ -95,3 +100,63 @@ def onOrderPaid(order, event):
                     expiry_date = now + datetime.timedelta(
                                             service.subscription_period)
                     ms.expiry_date = expiry_date
+
+
+def questionAsked(obj, event):
+    """ Deduct a credit when a question is asked
+    """
+    if is_expert(obj):
+        return
+
+    context = obj.relatedContent.to_object
+
+    service_uids = qaservice_uuids(context)
+    # there are no services so the user cannot pay for any.
+    if service_uids is None or len(service_uids) < 1:
+        return
+
+    memberservices = member_services(context, service_uids)
+    if len(memberservices) < 1:
+        raise RuntimeError("The user has no credits.")
+    else:
+        credits = memberservices[0].credits - 1
+        memberservices[0].credits = credits
+
+
+def questionDeleted(obj, event):
+    """ Add a credit when a question is deleted.
+    """
+    if is_expert(obj):
+        return
+
+    context = obj.relatedContent.to_object
+
+    service_uids = qaservice_uuids(context)
+    if service_uids is None or len(service_uids) < 1:
+        return
+
+    memberservices = member_services(context, service_uids)
+    if len(memberservices) < 1:
+        raise RuntimeError("The user has no credits.")
+    else:
+        credits = memberservices[0].credits + 1
+        memberservices[0].credits = credits
+
+
+def memberServiceAdded(obj, event):
+    service = obj.related_service.to_object
+    obj_path = '/'.join(obj.getPhysicalPath())
+
+    query = {'portal_type': 'emas.app.memberservice',
+             'memberid': obj.userid,
+             'serviceuid': IUUID(service),
+            }
+    pc = getToolByName(service, 'portal_catalog')
+    # exclude the current obj based on its path
+    brains = [b for b in pc(query) if b.getPath() != obj_path]
+    if brains is not None and len(brains) > 0:
+        raise RuntimeError(
+            'Only one memberservice is allowed per purchasable service.'
+            'This member (%s) already has a memberservice for '
+            '%s.' %(obj.userid, service.Title())
+        )
