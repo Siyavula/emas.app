3dca4c1f4c20fd449059a469ae213ebddb1dd38b
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-06-26 17:04:46 +0200

    Put order and invoice notification back.

--- emas/app/browser/purchase.py
+++ emas/app/browser/purchase.py
@@ -1,9 +1,15 @@
+from email.Utils import formataddr
+
 from five import grok
 from Acquisition import aq_inner
-
+from zope.component import queryUtility
 from zope.interface import Interface
 from z3c.relationfield.relation import create_relation
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+from plone.registry.interfaces import IRegistry
+
+from emas.theme.interfaces import IEmasSettings
 
 SERVICE_SELECTION      = 'service_selection'
 SELECTION_CONFIRMATION = 'selection_confirmation'
@@ -20,6 +26,8 @@ class Purchase(grok.View):
     grok.require('zope2.View')
     grok.name('purchase')
 
+    ordertemplate = ViewPageTemplateFile('templates/ordermailtemplate.pt')
+    ordernotification = ViewPageTemplateFile('templates/ordernotification.pt')
     
     def update(self):
         # set local variables for use in the template
@@ -61,24 +69,91 @@ class Purchase(grok.View):
                 title=order_id,
                 userid=memberid
             )
-            order = member_orders._getOb(order_id)
+            self.order = member_orders._getOb(order_id)
 
             for sid, quantity in order_items.items():
                 service = products_and_services[sid]
                 relation = create_relation(service.getPhysicalPath())
-                item_id = order.generateUniqueId(type_name='orderitem')
-                order.invokeFactory(
+                item_id = self.order.generateUniqueId(type_name='orderitem')
+                self.order.invokeFactory(
                     type_name='emas.app.orderitem',
                     id=item_id,
                     title=item_id,
                     related_item=relation,
                     quantity=quantity,
                 )
+            
+            self.send_invoice(self.order)
 
             self.request.response.redirect(
-                '@@paymentdetails/?orderid=%s' %order.getId())
+                '@@paymentdetails/?orderid=%s' %self.order.getId())
     
     def products_and_services(self):
         pps = self.context.restrictedTraverse('@@plone_portal_state')
         products_and_services = pps.portal()._getOb('products_and_services')
         return products_and_services.getFolderContents(full_objects=True)
+
+    def send_invoice(self, order): 
+        """ Send Invoice to recipients
+        """
+        registry = queryUtility(IRegistry)
+        settings = registry.forInterface(IEmasSettings)
+        state = self.context.restrictedTraverse('@@plone_portal_state')
+        portal = state.portal()
+        member = state.member()
+        host = portal.MailHost
+        encoding = portal.getProperty('email_charset')
+
+        send_from_address = formataddr(
+            ( 'Siyavula Education', settings.order_email_address )
+        )
+        
+        send_to_address = formataddr((member.getProperty('fullname'),
+                                      member.getProperty('email')))
+
+        subject = 'Order from %s Website' % state.navigation_root_title()
+
+        fullname=member.getProperty('fullname')
+        sitename=state.navigation_root_title()
+        items = order.order_items()
+        orderitems=[i.related_item.to_object.Title() for i in items]
+        totalcost=order.total()
+        username=member.getId()
+        ordernumber=order.getId()
+        email=settings.order_email_address
+        phone=settings.order_phone_number
+
+        # Generate message and attach to mail message
+        message = self.ordertemplate(
+            fullname=fullname,
+            sitename=sitename,
+            orderitems=orderitems,
+            totalcost=totalcost,
+            username=username,
+            ordernumber=ordernumber,
+            email=email,
+            phone=phone
+        )
+
+        portal.MailHost.send(message, send_to_address, send_from_address,
+                             subject, charset=encoding)
+
+        subject = 'New Order placed on %s Website' % \
+            state.navigation_root_title()
+
+        # Generate order notification
+        message = self.ordernotification(
+            fullname=fullname,
+            sitename=sitename,
+            orderitems=orderitems,
+            totalcost=totalcost,
+            orderurl=order.absolute_url(),
+            username=username,
+            ordernumber=ordernumber,
+            email=email,
+            phone=phone
+        )
+
+        # Siyavula's copy
+        portal.MailHost.send(message, send_from_address, send_from_address,
+                             subject, charset=encoding)
