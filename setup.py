ea99f8fa1a43f47d10db97b2162f66997d8ef523
Roché Compaan <roche@upfrontsystems.co.za>
2012-07-10 17:28:46 +0200

    use README.rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.1'
 setup(name='emas.app',
       version=version,
       description="emas.app",
-      long_description=open("README.md").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
