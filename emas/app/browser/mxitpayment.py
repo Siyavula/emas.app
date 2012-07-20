7020b7cbd0237d4238f85a1b6b3629a178847cac
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-07-20 13:55:06 +0200

    Update transaction reference on each call.

--- emas/app/browser/mxitpayment.py
+++ emas/app/browser/mxitpayment.py
@@ -3,8 +3,13 @@ from five import grok
 from Acquisition import aq_inner
 
 from zope.interface import Interface
+from zope.component import queryUtility
 
-    
+from plone.registry.interfaces import IRegistry
+
+from emas.theme.interfaces import IEmasSettings
+
+
 grok.templatedir('templates')
     
     
@@ -21,7 +26,13 @@ class MxitPaymentRequest(grok.View):
         self.base_url = 'http://billing.internal.mxit.com/Transaction/PaymentRequest'
         self.action = self.context.absolute_url() + '/@@mxitpaymentrequest'
         self.vendor_id = '1'
-        self.transaction_reference = '1'
+
+        registry = queryUtility(IRegistry)
+        self.settings = registry.forInterface(IEmasSettings)
+        self.transaction_reference = self.settings.order_sequence_number + 1
+        self.settings.order_sequence_number = self.transaction_reference
+        self.transaction_reference = '%04d' % self.transaction_reference
+
         self.callback_url = self.context.absolute_url() + '/mxitpaymentresponse'
         self.product_id = 'test product'
         self.product_name = 'test product'
