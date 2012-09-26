982e356da7dfdcf7f27e09e1f484082793fab48c
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-09-26 11:05:31 +0200

    Admin browser view to display memberservices for specific member

--- emas/app/browser/memberservices.py
+++ emas/app/browser/memberservices.py
@@ -5,6 +5,7 @@ from zope.interface import Interface
 
 from emas.app.browser.utils import practice_service_uuids
 from emas.app.browser.utils import member_services
+from emas.app.browser.utils import member_services_for
 from emas.app.browser.utils import service_url as get_service_url
     
 grok.templatedir('templates')
@@ -24,3 +25,23 @@ class MemberServices(grok.View):
 
     def service_url(self, service):
         return get_service_url(service)
+
+    
+class ActiveMemberServicesFor(grok.View):
+    """ Show all memberservices for a specific user.
+        Used as administration reporting tool.
+    """
+    
+    grok.context(Interface)
+    grok.require('zope2.View')
+    grok.name('active-memberservices-for')
+
+    def update(self):
+        self.memberservices = []
+        self.userid = self.request.get('userid', '')
+        if self.userid:
+            uids = practice_service_uuids(self.context)
+            self.memberservices =  member_services_for(self.context,
+                                                       uids,
+                                                       self.userid)
+
