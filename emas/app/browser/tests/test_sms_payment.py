3f7ce144000aa05a3b34e4805437643955a24422
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-10-08 19:03:04 +0200

    Get going with tests.

--- emas/app/browser/tests/test_sms_payment.py
+++ emas/app/browser/tests/test_sms_payment.py
@@ -1,22 +1,19 @@
 import unittest
 
-from zope.component import createObject
-from zope.component import queryUtility
-
-from plone.dexterity.interfaces import IDexterityFTI
-
 from Products.PloneTestCase.ptc import PloneTestCase
+
 from emas.app.tests.layer import Layer
 
-from emas.app.order import IOrder
-
-
-class TestSMSPaymentIntegration(PloneTestCase):
+class TestSMSPaymentApprovedView(PloneTestCase):
     
     layer = Layer
     
-    def test_prepSMS(self):
-        self.fail('Nothing tested yet.')
-    
+    def test_confirm_view_exists(self):
+        view = self.portal.restrictedTraverse('@@smspaymentapproved')
+        assert view is not None, '@@smspaymentapproved view is missing.'
+
+    def test_purchase_approved(self):
+        self.fail('Not implemented yet.')
+
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
