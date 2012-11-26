73acff04b88eecdfe3b5e0e3ca85ce1e9cd20630
Rijk Stofberg <rijk@upfrontsystems.co.za>
2012-11-26 23:27:03 +0200

    Log all exceptions so we can figure out what goes wrong.

--- emas/app/browser/vcsresponseprocessors.py
+++ emas/app/browser/vcsresponseprocessors.py
@@ -40,31 +40,35 @@ class PaymentApproved(grok.View):
 
     
     def update(self):
-        self.logDetails()
-        self.pps = self.context.restrictedTraverse('@@plone_portal_state')
-        pms = getToolByName(self.context, 'portal_membership')
+        try:
+            self.logDetails()
+            self.pps = self.context.restrictedTraverse('@@plone_portal_state')
+            pms = getToolByName(self.context, 'portal_membership')
 
-        oid = self.request.get('p2')
-        self.order = getOrder(self.context, self.request)
+            oid = self.request.get('p2')
+            self.order = getOrder(self.context, self.request)
 
-        member = pms.getMemberById(self.order.userid)
-        newSecurityManager(self.request, member)
-        wf = getToolByName(self.context, 'portal_workflow')
-        status = wf.getStatusOf('order_workflow', self.order)
-        if status['review_state'] != 'paid':
-            wf.doActionFor(self.order, 'pay')
-            self.order.reindexObject()
+            member = pms.getMemberById(self.order.userid)
+            newSecurityManager(self.request, member)
+            wf = getToolByName(self.context, 'portal_workflow')
+            status = wf.getStatusOf('order_workflow', self.order)
+            if status['review_state'] != 'paid':
+                wf.doActionFor(self.order, 'pay')
+                self.order.reindexObject()
 
-        # we put 'm_1', the absolute_url of the context, in as parameter to the
-        # initial VCS call.  If it is returned we want to show the approved page
-        # in that context rather than the current context, which could be the
-        # root of the plone site.
-        original_url = self.request.get('m_1', None)
-        if original_url is not None and len(original_url) > 0:
-            # add p2 as url parameter since we are redirecting back to
-            # the same view and need to look up the order again
-            url = original_url + '/@@paymentapproved?p2=%s' % oid
-            self.request.response.redirect(url)
+            # we put 'm_1', the absolute_url of the context, in as parameter to the
+            # initial VCS call.  If it is returned we want to show the approved page
+            # in that context rather than the current context, which could be the
+            # root of the plone site.
+            original_url = self.request.get('m_1', None)
+            if original_url is not None and len(original_url) > 0:
+                # add p2 as url parameter since we are redirecting back to
+                # the same view and need to look up the order again
+                url = original_url + '/@@paymentapproved?p2=%s' % oid
+                self.request.response.redirect(url)
+        except Exception, ex:
+            LOGGER.warn(ex)
+            self.context.restrictedTraverse('@@member-services')()
     
     def memberservices(self):
         return get_display_items_from_order(self.order)
