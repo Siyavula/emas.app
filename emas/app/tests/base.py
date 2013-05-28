e9725115bbd18d0556d73364268af81ef4bb4ca0
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-05-28 21:32:03 +0200

    Use plone.app.testing

--- emas/app/tests/base.py
+++ emas/app/tests/base.py
@@ -1,85 +1,57 @@
-"""Test setup for integration and functional tests.
-"""
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
 
-from Products.Five import zcml
-from Products.Five import fiveconfigure
+class Fixture(PloneSandboxLayer):
 
-from Testing import ZopeTestCase as ztc
+    defaultBases = (PLONE_FIXTURE,)
 
-from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import onsetup
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import plone.app.registry
+        import plone.resource
+        import plone.app.theming
+        import plone.app.folder
+        import rhaptos.xmlfile
+        import collective.topictree
+        import webcouturier.dropdownmenu
+        import emas.transforms
+        import rhaptos.cnxmltransforms
+        import rhaptos.compilation
+        import upfront.shorturl
+        import fullmarks.mathjax
+        import siyavula.what
+        import inqbus.plone.fastmemberproperties
+        import emas.app
 
-#
-# When ZopeTestCase configures Zope, it will *not* auto-load products in 
-# Products/. Instead, we have to use a statement such as:
-# 
-#   ztc.installProduct('SimpleAttachment')
-# 
-# This does *not* apply to products in eggs and Python packages (i.e. not in
-# the Products.*) namespace. For that, see below.
-# 
-# All of Plone's products are already set up by PloneTestCase.
-# 
+        self.loadZCML(package=plone.app.registry)
+        self.loadZCML(package=plone.resource)
+        self.loadZCML(package=plone.app.theming)
+        self.loadZCML(package=plone.app.folder)
+        self.loadZCML(package=rhaptos.xmlfile)
+        self.loadZCML(package=collective.topictree)
+        self.loadZCML(package=webcouturier.dropdownmenu)
+        self.loadZCML(package=emas.transforms)
+        self.loadZCML(package=rhaptos.cnxmltransforms)
+        self.loadZCML(package=rhaptos.compilation)
+        self.loadZCML(package=upfront.shorturl)
+        self.loadZCML(package=fullmarks.mathjax)
+        self.loadZCML(package=siyavula.what)
+        self.loadZCML(package=inqbus.plone.fastmemberproperties)
+        self.loadZCML(package=emas.app)
 
-@onsetup
-def setup_product():
-    """Set up the package and its dependencies.
-    
-    The @onsetup decorator causes the execution of this body to be deferred
-    until the setup of the Plone site testing layer. We could have created our
-    own layer, but this is the easiest way for Plone integration tests.
-    """
-    
-    fiveconfigure.debug_mode = True
 
-    import plone.app.registry
-    import plone.resource
-    import plone.app.theming
-    import plone.app.folder
-    import rhaptos.xmlfile
-    import collective.topictree
-    import webcouturier.dropdownmenu
-    import emas.transforms
-    import rhaptos.cnxmltransforms
-    import rhaptos.compilation
-    import upfront.shorturl
-    import fullmarks.mathjax
-    import siyavula.what
-    import inqbus.plone.fastmemberproperties
-    import emas.app
+    def setUpPloneSite(self, portal):
+        self.applyProfile(portal, 'emas.app:default')
 
-    zcml.load_config('configure.zcml', package=plone.app.registry)
-    zcml.load_config('configure.zcml', package=plone.resource)
-    zcml.load_config('configure.zcml', package=plone.app.theming)
-    zcml.load_config('configure.zcml', package=rhaptos.xmlfile)
-    zcml.load_config('configure.zcml', package=collective.topictree)
-    zcml.load_config('configure.zcml', package=webcouturier.dropdownmenu)
-    zcml.load_config('configure.zcml', package=emas.transforms)
-    zcml.load_config('configure.zcml', package=rhaptos.cnxmltransforms)
-    zcml.load_config('configure.zcml', package=rhaptos.compilation)
-    zcml.load_config('configure.zcml', package=upfront.shorturl)
-    zcml.load_config('configure.zcml', package=fullmarks.mathjax)
-    zcml.load_config('configure.zcml', package=siyavula.what)
-    zcml.load_config('configure.zcml',
-                     package=inqbus.plone.fastmemberproperties)
-    zcml.load_config('configure.zcml', package=emas.app)
 
-    fiveconfigure.debug_mode = False
-    
-    ztc.installPackage('emas.app')
-    
-# The order here is important: We first call the (deferred) function which
-# installs the products we need for this product. Then, we let PloneTestCase 
-# set up this product on installation.
-
-setup_product()
-ptc.setupPloneSite(products=['emas.theme'])
-
-class BaseTestCase(ptc.PloneTestCase):
-    """
-    """
-
-class BaseFunctionalTestCase(ptc.FunctionalTestCase):
-    """
-    """
-
+FIXTURE = Fixture()
+INTEGRATION_TESTING = IntegrationTesting(
+    bases=(FIXTURE,),
+    name='emas.app:Integration',
+    )
+FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(FIXTURE,),
+    name='emas.app:Functional',
+    )
