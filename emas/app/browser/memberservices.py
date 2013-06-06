f11a16af6050baa216dfe691d3fcedbe0662e09b
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-06-06 21:27:44 +0200

    Get the memberservices from utils (which gets it from the database)

--- emas/app/browser/memberservices.py
+++ emas/app/browser/memberservices.py
@@ -1,37 +1,32 @@
 from five import grok
-from Acquisition import aq_inner
 
 from zope.interface import Interface
 
 from emas.app.browser.utils import practice_service_uuids
 from emas.app.browser.utils import member_services
-from emas.app.browser.utils import member_services_for
 from emas.app.browser.utils import service_url as get_service_url
-    
+
+
 grok.templatedir('templates')
-    
-    
+
 class MemberServices(grok.View):
     """ Returns all the authenticated member's services
     """
     
     grok.context(Interface)
     grok.require('zope2.View')
-    grok.name('member-services')
+    grok.name('db-member-services')
 
     def update(self):
-        uids = practice_service_uuids(self.context)
-        self.memberservices =  member_services(self.context, uids)
+        self.service_uids = practice_service_uuids(self.context)
+        self.memberservices = []
+        self.memberservices = member_services(self.context, self.service_uids)
 
     def service_url(self, service):
-        return get_service_url(service)
+        return ''
 
-    
+"""
 class ActiveMemberServicesFor(grok.View):
-    """ Show all memberservices for a specific user.
-        Used as administration reporting tool.
-    """
-    
     grok.context(Interface)
     grok.require('zope2.View')
     grok.name('active-memberservices-for')
@@ -44,4 +39,4 @@ class ActiveMemberServicesFor(grok.View):
             self.memberservices =  member_services_for(self.context,
                                                        uids,
                                                        self.userid)
-
+"""
