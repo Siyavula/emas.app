a865ae397ef99efa241df5f2251ab48bb9f85931
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-06-09 12:45:59 +0200

    Test adding of new memberservices.

--- emas/app/tests/test_member_services.py
+++ emas/app/tests/test_member_services.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+
 import unittest2 as unittest
 
 from z3c.relationfield.relation import create_relation
 from zope.component import createObject
 from zope.component import queryUtility
+from zope.intid.interfaces import IIntIds
 
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
@@ -10,6 +13,7 @@ from plone.app.testing import setRoles
 from plone.dexterity.interfaces import IDexterityFTI
 
 from emas.app.member_service import IMemberService
+from emas.app.browser import utils
 from emas.app.tests.base import INTEGRATION_TESTING
 
 
@@ -33,30 +37,27 @@ class TestMemberServiceIntegration(unittest.TestCase):
         self.service = self.folder._getOb(s_id)
     
     def test_adding(self):
-        related_service = create_relation(self.service.getPhysicalPath())
-        ms_id = self.folder.invokeFactory('emas.app.memberservice',
-                                          'memberservice1',
-                                          userid=TEST_USER_ID,
-                                          related_service=related_service,
-                                          service_type='subscription')
-        ms1 = self.folder._getOb(ms_id)
+        intids = queryUtility(IIntIds, context=self.portal)
+        kwargs = {'memberid': TEST_USER_ID,
+                  'title': '%s for %s' % (self.service.title, TEST_USER_ID),
+                  'related_service_id': intids.getId(self.service),
+                  'expiry_date': datetime.now(),}
+        ms1= utils.add_memberservice(self.portal, **kwargs) 
         self.failUnless(IMemberService.providedBy(ms1))
 
     def test_fti(self):
         fti = queryUtility(IDexterityFTI, name='emas.app.memberservice')
-        self.assertNotEquals(None, fti)
+        self.assertEquals(None, fti)
 
     def test_schema(self):
         fti = queryUtility(IDexterityFTI, name='emas.app.memberservice')
         schema = fti.lookupSchema()
-        self.assertEquals(IMemberService, schema)
+        self.assertEquals(None, schema)
 
     def test_factory(self):
         fti = queryUtility(IDexterityFTI, name='emas.app.memberservice')
         factory = fti.factory
-        new_object = createObject(factory)
-        self.failUnless(IMemberService.providedBy(new_object))
-
+        self.assertEquals(factory, None)
 
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
