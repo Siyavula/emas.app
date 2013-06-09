b04123cce0bd27bef12c6fa1e8a30096d0e7965b
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-06-09 12:44:59 +0200

    Add marker interface for our new content class. Fix column name.

--- emas/app/alchemy/memberservice.py
+++ emas/app/alchemy/memberservice.py
@@ -1,15 +1,25 @@
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 
+from zope.interface import Interface, implements
+
+class IMemberService(Interface):
+    """
+        Marker interface.
+    """
+
+
 Base = declarative_base()
 class MemberService(Base):
+    
+    implements(IMemberService)
 
     __tablename__ = 'memberservices'
 
-    id              = Column('memberservice_id', Integer, primary_key=True)
-    userid          = Column('userid', String(100))
-    title           = Column('title', String(100))
-    related_service = Column('related_service', Integer)
-    expiry_date     = Column('expiry_date', Date)
-    credits         = Column('credits', Integer)
-    service_type    = Column('service_type', String(100))
+    id                 = Column('memberservice_id', Integer, primary_key=True)
+    userid             = Column('userid', String(100))
+    title              = Column('title', String(100))
+    related_service_id = Column('related_service_id', Integer)
+    expiry_date        = Column('expiry_date', Date)
+    credits            = Column('credits', Integer)
+    service_type       = Column('service_type', String(100))
