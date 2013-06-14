b9b3360d58d810a02b7898019d36c9381fba1e79
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-06-14 12:18:48 +0200

    Fiddle with zope.testbrowser and testrecorder.

--- emas/app/tests/test_eventhandlers.py
+++ emas/app/tests/test_eventhandlers.py
@@ -1,14 +1,29 @@
-import os
+import unittest2 as unittest
 
-from base import BaseFunctionalTestCase
+from plone.testing.z2 import Browser as z2Browser
+from zope.testbrowser.browser import Browser as ZopeTestBrowser
 
-dirname = os.path.dirname(__file__)
+from emas.app.tests.base import FUNCTIONAL_TESTING 
 
-class TestEventhandlers(BaseFunctionalTestCase):
-    """ Test the intelligent practice messages service viewlets  """
-    
-    def setUp(self):
-        super(TestEventhandlers, self).setUp()
-    
-    def test_eventhandlers(self):
-        self.fail()
+class MemberServiceFunctionalTests(unittest.TestCase):
+
+    layer =  FUNCTIONAL_TESTING
+
+    def test_register(self):
+        self.portal = self.layer['portal']
+        self.app = self.layer['app']
+        import pdb;pdb.set_trace()
+
+        #browser = ZopeTestBrowser()
+        #browser.open('http://nohost/plone/@@register')
+        #browser.getControl('Full Name').value = 'tester086'
+        #browser.getControl('E-mail').value = 'rijk@upfrontsystems.co.za'
+        #browser.getControl('Confirm password').value = '12345'
+        #browser.getControl('Subscibe to newsletter').selected = False
+        #browser.getControl('Register').click()
+
+        browser = z2Browser(self.app)
+        browser.open(self.portal.portal_url() + '/register')
+        assert('register' in browser.contents,
+               'This is not the registration page.')
+
