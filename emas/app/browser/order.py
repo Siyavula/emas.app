d61e5c5db34aab748f7762b90061f7192412b2e8
Roché Compaan <roche@upfrontsystems.co.za>
2013-07-11 20:07:00 +0200

    remove inputs no longer need for new order form
    use bootstrap style for submit button

--- emas/app/browser/order.py
+++ emas/app/browser/order.py
@@ -17,22 +17,12 @@ class Order(grok.View):
     grok.require('zope2.View')
 
     def __call__(self):
-        self.grade = None
         self.prod_practice_book = None
-        self.subjects = None
-        if self.request.has_key('order.form.submitted'):
-            # validate form
-            self.grade = self.request.form.get('grade', None)
-            self.prod_practice_book = self.request.form.get('prod_practice_book', None)
-            self.subjects = self.request.form.get('subjects', None)
-            names = ['selected_subjects', 'selected_grade', 'selected_product']
-            self.missing_fields = \
-                [name for name in names if getattr(self, name, None) is None]
-        
-            if self.grade and self.prod_practice_book and self.subjects:
-                # traverse to confirm if form has required fields
-                view = self.context.restrictedTraverse('@@confirm')
-                return view()
+        self.subjects = self.request.get('subjects', None)
+        if self.request.has_key('order.form.submitted') and self.subjects:
+            # traverse to confirm if form has required fields
+            view = self.context.restrictedTraverse('@@confirm')
+            return view()
                 
         return super(Order, self).__call__()
 
@@ -42,7 +32,7 @@ class Order(grok.View):
             if pmt.isAnonymousUser():
                 self.context.restrictedTraverse('logged_in')()
 
-            if not(self.grade and self.prod_practice_book and self.subjects):
+            if not self.subjects:
                 pps = self.context.restrictedTraverse('@@plone_portal_state')
                 ptool = pps.portal().plone_utils
                 ptool.addPortalMessage('All fields are required.', 'warning')
@@ -65,38 +55,14 @@ class Order(grok.View):
         """
         return '%s/@@order' %self.context.absolute_url()
         
-    def subjects(self):
-        return self.request.get('subjects', '')
-   
     def subject_selected(self, subject, selected):
         return subject == selected and 'checked' or ''
-
-    def grade(self):
-        return self.request.get('grade', '')
-
-    def grade_selected(self, grade, selected):
-        return grade == selected and 'checked' or ''
-
-    def prod_practice_book(self):
-        return self.request.get('prod_practice_book', '')
-
-    def prod_practice_book_selected(self, prod_practice_book, selected):
-        return prod_practice_book == selected and 'checked' or ''
 
     def prod_payment(self):
         return self.request.get('prod_payment', '')
 
     def prod_payment_selected(self, prod_payment, selected):
         return prod_payment == selected and 'checked' or ''
-
-    def fullname(self):
-        return self.request.get('fullname', '')
-
-    def phone(self):
-        return self.request.get('phone', '')
-
-    def shipping_address(self):
-        return self.request.get('shipping_address', '')
 
     def ordernumber(self):
         return self.request.get('ordernumber', '')
