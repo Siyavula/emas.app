97989d8e8bc50fae882ef56eb57b2364595f9a6a
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-07-12 12:28:47 +0200

    Remove over complicated clause generation in favour of zope record marshalling.

--- emas/app/browser/list_orders.py
+++ emas/app/browser/list_orders.py
@@ -1,3 +1,6 @@
+import datetime
+from types import DictType
+
 from five import grok
 from Acquisition import aq_inner
 
@@ -16,67 +19,13 @@ from emas.app import MessageFactory as _
 grok.templatedir('templates')
 
 
-class FieldIndexClause(object):
-    def get(self, criterion, request):
-        value = request.get(criterion)
-        if not value:
-            return None
-        return {criterion: value}
-
-class KeywordIndexClause(object):
-    def get(self, criterion, request):
-        values = request.get(criterion)
-        if not values:
-            return None
-        return {criterion: [values]}
-
-class StartDateIndexClause(object):
-    def get(self, criterion, request):
-        value = request.get(criterion)
-        if not value:
-            return None
-        return {'ordered_date': {'min': date}}
-
-class EndDateIndexClause(object):
-    def get(self, criterion, request):
-        value = request.get(criterion)
-        if not value:
-            return None
-        return {'ordered_date': {'max': date}}
-
-class DateRangeClause(object):
-    def get(self, criterion, request):
-        ordered_date = request.get('ordered_date', None)
-        if not ordered_date:
-            return None
-        max_date = ordered_date.end
-        min_date = ordered_date.start
-        if max_date and min_date:
-            return {'order_date': {'minmax': [min_date, max_date]}}
-        else:
-            return None
-
-
-FIELDINDEX_CLAUSE = FieldIndexClause() 
-KEYWORD_CLAUSE = KeywordIndexClause()
-START_DATE_INDEX_CLAUSE = StartDateIndexClause()
-END_DATE_INDEX_CLAUSE = EndDateIndexClause()
-DATERANGE_CLAUSE = DateRangeClause()
-FILTERS = {'review_state'        : FIELDINDEX_CLAUSE,
-           'payment_method'      : FIELDINDEX_CLAUSE,
-           'userid'              : FIELDINDEX_CLAUSE,
-           'related_item_uuids'  : KEYWORD_CLAUSE,
-           'getId'               : FIELDINDEX_CLAUSE,
-           'id'                  : FIELDINDEX_CLAUSE,
-           'order_date'          : DATERANGE_CLAUSE,
-           'order_number'        : FIELDINDEX_CLAUSE,
-           'verification_code'   : FIELDINDEX_CLAUSE,}
-
-class List_Orders(grok.View, FolderContentsView):
+class List_Orders(grok.View):
     """
     """
     grok.context(IOrderFolder)
     grok.require('zope2.View')
+
+    title = 'Orders'
 
     def update(self):
         pps = self.context.restrictedTraverse('@@plone_portal_state')
@@ -92,11 +41,24 @@ class List_Orders(grok.View, FolderContentsView):
 
             query = {'portal_type': 'emas.app.order',
                      'path': '/'.join(self.orders_folder.getPhysicalPath())}
-            for criterion, clause_generator in FILTERS.items():
-                clause = clause_generator.get(criterion, self.request)
-                if clause:
-                    query.update(clause)
-                    got_filter_criteria = True
+            
+            # make it a dict, because I want an easy way to get rid of some keys
+            filter_criteria = DictType(self.request['filter_criteria'])
+            if filter_criteria:
+                got_filter_criteria = True
+
+            now = datetime.datetime.now()
+            yesterday = now - datetime.timedelta(1)
+            tomorrow = now + datetime.timedelta(1)
+
+            start_date = filter_criteria.get('order_date_start', yesterday)
+            filter_criteria.pop('order_date_start', None)
+            end_date = filter_criteria.get('order_date_end', tomorrow)
+            filter_criteria.pop('order_date_end', None)
+            query['start_date'] = {'minmax': [start_date, end_date]}
+            
+            for key, value in filter_criteria.items():
+                query[key] = value
             
             if got_filter_criteria:
                 self.orders = oc(query)
@@ -124,7 +86,9 @@ class List_Orders(grok.View, FolderContentsView):
         return services
     
     def get_review_state_for(self, order):
-        return 'paid'
+        wft = getToolByName(self.context, 'portal_workflow')
+        status = wft.getStatusOf('order_workflow', order) 
+        return status['review_state']
 
     def get_related_services_for(self, order):
         services = []
