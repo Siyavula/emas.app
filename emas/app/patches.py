97cbc92658112e180ead19e222922c71df9d7aba
Rijk Stofberg <rijk.stofberg@gmail.com>
2013-12-04 19:22:53 +0200

    Set the login time only the very first time a member logs in.

--- emas/app/patches.py
+++ emas/app/patches.py
@@ -1,10 +1,22 @@
+from DateTime import DateTime
 from Products.PlonePAS.tools.membership import MembershipTool
+from AccessControl.SecurityManagement import getSecurityManager
 
 
 def setLoginTimes(self):
     """ Changed into a no-op in order to stop ZODB access on each login.
         It is unnecessary and kills the app's performance.
     """ 
-    return False
+    initialLogin = False
+    member = getSecurityManager().getUser()
+    if member is not None:
+        default = DateTime('2000/01/01')
+        login_time = member.getProperty('login_time', default)
+        if login_time == default:
+            initialLogin = True
+	    login_time = DateTime()
+            member.setProperties(login_time=self.ZopeTime(),
+                                 last_login_time=login_time)
+    return initialLogin
 
 MembershipTool.setLoginTimes = setLoginTimes
